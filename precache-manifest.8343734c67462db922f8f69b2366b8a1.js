f990006bbadad2b8621378da05a7dd87f5763923
maqweb <balikhinv@bk.ru>
2019-12-24 10:36:40 +0300

    Updates

---
+++ precache-manifest.8343734c67462db922f8f69b2366b8a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cb13e0e41c4e080891fd3ac73b6180",
+    "url": "/counter-react/index.html"
+  },
+  {
+    "revision": "38c6afe23f3b49dc0d0f",
+    "url": "/counter-react/static/css/main.c026bec5.chunk.css"
+  },
+  {
+    "revision": "d737c9649199d99c7f8e",
+    "url": "/counter-react/static/js/2.f74456ab.chunk.js"
+  },
+  {
+    "revision": "38c6afe23f3b49dc0d0f",
+    "url": "/counter-react/static/js/main.8354c215.chunk.js"
+  },
+  {
+    "revision": "c6117624310e2fc92f07",
+    "url": "/counter-react/static/js/runtime-main.6b8287f2.js"
+  }
+]);
