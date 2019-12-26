9ebb515dcc773c1c3b9058cdf5cf222da7947b6c
maqweb <balikhinv@bk.ru>
2019-12-27 00:21:48 +0300

    Updates

--- precache-manifest.1359b967fa97bf2960454ab838852921.js
+++ precache-manifest.1359b967fa97bf2960454ab838852921.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b4a327b8585a3b30a34a7d929bddd551",
+    "revision": "f310d293abfa98d6336c6febce4540de",
     "url": "/counter-react/index.html"
   },
   {
-    "revision": "ed3e7e5d5896adda3d63",
-    "url": "/counter-react/static/css/main.8009dd87.chunk.css"
+    "revision": "f0ac31d9ec3c1c9eda4e",
+    "url": "/counter-react/static/css/main.d6c1e969.chunk.css"
   },
   {
     "revision": "d737c9649199d99c7f8e",
     "url": "/counter-react/static/js/2.f74456ab.chunk.js"
   },
   {
-    "revision": "ed3e7e5d5896adda3d63",
-    "url": "/counter-react/static/js/main.27f07113.chunk.js"
+    "revision": "f0ac31d9ec3c1c9eda4e",
+    "url": "/counter-react/static/js/main.2edbf623.chunk.js"
   },
   {
     "revision": "c6117624310e2fc92f07",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/counter-react/precache-manifest.2c8e28df9a1da6e2488bd018a505557e.js"
+  "/counter-react/precache-manifest.1359b967fa97bf2960454ab838852921.js"
 );
 
 self.addEventListener('message', (event) => {
